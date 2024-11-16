6265eb6291f0da1ca5d47fd19207ce22b328c5b4
Monisha GaneshKumar <monisha090301@gmail.com>
2024-11-16 12:08:09 +0530

    First commit

---
+++ CypressAutomation/cypress/e2e/MyFirstTest.cy.js
@@ -0,0 +1,9 @@
+describe('My First Test', () => {  // this ia a test suite
+    it('test1', () => {   // this is one of the testcase
+    
+       cy.visit("https://demoqa.com/automation-practice-form");
+
+
+    })
+
+  })
